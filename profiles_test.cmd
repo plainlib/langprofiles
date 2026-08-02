69b72160305b1f8fcdfa1a09273d5af5b1f1db93
Alexander Tverskoy <astverskoy@gmail.com>
2026-08-02 11:41:04 +0300

    Detailed the test

--- profiles_test.cmd
+++ profiles_test.cmd
@@ -1,4 +1,7 @@
-langprofiles 10 20
-langprofiles 30 15
+langprofiles 10 10
+langprofiles 20 10
+langprofiles 30 10
+langprofiles 40 10
+langprofiles 50 10
 langprofiles 100 10
-langprofiles 1000 3
+langprofiles 1000 10
